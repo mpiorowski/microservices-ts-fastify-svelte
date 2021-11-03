7c688896e5aaa492fdd4e709c51642c23821cf6e
mateuszpiorowski <mateuszpiorowski@gmail.com>
2021-11-03 23:14:21 +0100

    cleaning

--- start-front.sh
+++ start-front.sh
@@ -1,1 +1,2 @@
+yarn --cwd ./client
 yarn --cwd ./client dev
--- start-reset.sh
+++ start-reset.sh
@@ -1,2 +1,4 @@
 yarn --cwd ./service-users migrate reset
 yarn --cwd ./service-chat migrate reset
+
+docker container stop $(docker container ls -aq) && docker container rm $(docker container ls -aq)
